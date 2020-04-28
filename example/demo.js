cf97364f89c5d7733a1e5642ed03183e7e9f7d51
woaidouya123 <cfeigood@qq.com>
2020-04-28 16:09:11 +0800

    增加欢迎和礼物请求显示

--- example/demo.js
+++ example/demo.js
@@ -2,7 +2,22 @@ import BILIWS from '../src/index'
 
 const dom = window.document
 let room = new BILIWS(4350043)
-room.$start().$subscribe(addDanMu)
+room.$start().$subscribe((msg)=>{
+  console.log(msg)
+  switch(msg.cmd){
+    case 'WELCOME':
+      welcome(msg)
+      break
+    case 'DANMU_MSG':
+      addDanMu(msg)
+      break
+    case 'SEND_GIFT':
+      sendGift(msg)
+      break;
+    default:
+      break
+  }
+})
 document.getElementById('btn').addEventListener('click', function () {
   const number = document.getElementById('input').value
   if (number) {
@@ -17,6 +32,20 @@ document.getElementById('btn').addEventListener('click', function () {
 function addDanMu (res) {
   const div = dom.createElement('div')
   div.setAttribute('class', 'danmu')
-  div.innerText = new Date().toLocaleTimeString() + ' $ ' + res.name + '   :   ' + res.text
+  div.innerText = new Date().toLocaleTimeString() + ' $ ' + res.info[2][1] + '   :   ' + res.info[1]
+  document.body.appendChild(div)
+}
+
+function welcome (res) {
+  const div = dom.createElement('div')
+  div.setAttribute('class', 'danmu')
+  div.innerText = new Date().toLocaleTimeString() + ' $ 欢迎 '+res.data.uname+' 来到直播间'
+  document.body.appendChild(div)
+}
+
+function sendGift (res) {
+  const div = dom.createElement('div')
+  div.setAttribute('class', 'danmu')
+  div.innerText = new Date().toLocaleTimeString() + ' $ 感谢 '+res.data.uname+' 赠送的 '+res.data.num+"个"+res.data.giftName
   document.body.appendChild(div)
 }
